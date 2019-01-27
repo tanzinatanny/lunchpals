b62e09feaf3e9e5a77f955497ac314427bb2fed0
jtishler <jenna@tishler.net>
2019-01-26 21:07:13 -0500

    navigation drawer navigates

--- app/src/main/java/com/hack/brown/lunchpals/MainActivity.java
+++ app/src/main/java/com/hack/brown/lunchpals/MainActivity.java
@@ -1,17 +1,21 @@
 package com.hack.brown.lunchpals;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener,
+        MessagesFragment.OnFragmentInteractionListener, ScheduleFragment.OnFragmentInteractionListener,
+            SettingsFragment.OnFragmentInteractionListener {
 
     private DrawerLayout mDrawerLayout;
 
@@ -20,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, new MainActivityFragment()).commit();
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -33,6 +38,94 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+
+                        return true;
+                    }
+                });
+
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+
+        Fragment fragment = null;
+
+        Class fragmentClass;
+
+        switch(menuItem.getItemId()) {
+
+            case R.id.nav_profile:
+
+                fragmentClass = ProfileFragment.class;
+
+                break;
+
+            case R.id.nav_messages:
+
+                fragmentClass = MessagesFragment.class;
+
+                break;
+
+            case R.id.nav_scheduling:
+
+                fragmentClass = ScheduleFragment.class;
+
+                break;
+
+            case R.id.nav_settings:
+
+                fragmentClass = SettingsFragment.class;
+
+                break;
+
+            default:
+
+                fragmentClass = MainActivityFragment.class;
+
+        }
+
+
+
+        try {
+
+            fragment = (Fragment) fragmentClass.newInstance();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+
+
+        // Insert the fragment by replacing any existing fragment
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+
+
+        // Highlight the selected item has been done by NavigationView
+
+        menuItem.setChecked(true);
+
+        // Set action bar title
+
+        setTitle(menuItem.getTitle());
+
+        // Close the navigation drawer
+
+        mDrawerLayout.closeDrawers();
+
     }
 
     @Override
@@ -55,5 +148,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
